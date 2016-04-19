637c361c2019d9374fe30b057f1d4e654efdf18e
Bartosz Owczarek <bowczarek@objectivity.co.uk>
2016-04-19 08:50:34 +0200

    Updated leaflet version to 0.7.7.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Leaflet from 'leaflet';
 
 export function configure(frameworkConfig, {
-    LeafletDefaultImagePath = 'jspm_packages/github/Leaflet/Leaflet@0.7.3/dist/images'
+    LeafletDefaultImagePath = 'jspm_packages/github/Leaflet/Leaflet@0.7.7/dist/images'
   } = {}) {
     Leaflet.Icon.Default.imagePath = LeafletDefaultImagePath;
     frameworkConfig.globalResources('./leaflet');
